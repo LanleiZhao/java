85526c0290356b8edc347a9e5246df79d1ba441b
lucas <1903076902@qq.com>
2020-07-06 01:04:15 +0800

    new file

---
+++ src/day28/testGit.java
@@ -0,0 +1,11 @@
+package day28;
+
+/**
+ * @author lucas
+ * @create 2020-07-06-01:03
+ */
+public class testGit {
+    public static void main(String[] args) {
+        System.out.println("github test");
+    }
+}
